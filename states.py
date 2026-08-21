6a685d085f461824c115a779a0a1564445edac46
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-08-21 05:16:27 +0500

    Create states.py

---
+++ states.py
@@ -0,0 +1,18 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class RegisterForm(StatesGroup):
+    name = State()
+    phone = State()
+    referral_code = State()
+
+class AddProductForm(StatesGroup):
+    name = State()
+    description = State()
+    price = State()
+    stock = State()
+    category = State()
+    subcategory = State()
+    gender = State()
+    size = State()
+    color = State()
+    image = State()
